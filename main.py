5fd8c077ab9108eca877cefc7df9cce68fafc514
Eric Lin <eric.lin@consensys.net>
2022-12-16 12:06:59 +1000

    specify directory cert
    Signed-off-by: Eric Lin <eric.lin@consensys.net>

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ aConfiguration = client.Configuration()
 
 with open("/run/secrets/kubernetes.io/serviceaccount/token", "r") as f:
     aToken = f.readline()
-with open("/run/secrets/kubernetes.io/serviceaccount/ca.crt", "r") as f:
-    aConfiguration.ssl_ca_cert = f.readline()
 
 aConfiguration.host = "https://kubernetes.default.svc:443"
+aConfiguration.ssl_ca_cert = "/run/secrets/kubernetes.io/serviceaccount/ca.crt"
+
 aConfiguration.verify_ssl = True
 
 aConfiguration.api_key = {"authorization": "Bearer " + aToken}
